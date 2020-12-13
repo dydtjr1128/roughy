20698368dc8b3607afaf3b6778c216ab0dc0da11
Yongseok Choi <dydtjr1994@gmail.com>
2020-12-13 15:59:21 +0900

    Add: 기본적인 onTab 이벤트 기능 구현

--- lib/component/templateContainer.dart
+++ lib/component/templateContainer.dart
@@ -1,8 +1,35 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class TemplateContainer extends StatelessWidget {
+  final Function onTap;
+  final Widget child;
+  final int containerIndex;
+  final BorderRadius _baseBorderRadius = BorderRadius.circular(8);
+
+  TemplateContainer(
+      {@required this.onTap,
+      @required this.child,
+      @required this.containerIndex});
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Card(
+      elevation: 2,
+      color: Colors.teal.shade200,
+      shape: RoundedRectangleBorder(borderRadius: _baseBorderRadius),
+      margin: EdgeInsets.symmetric(vertical: 7.0, horizontal: 7.0),
+      child: InkWell(
+        borderRadius: _baseBorderRadius,
+        onTap: () => onTap(containerIndex, context),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: _baseBorderRadius,
+            color: Colors.transparent,
+          ),
+          child: child,
+        ),
+      ),
+    );
   }
 }
--- lib/tab/templateSelectTab.dart
+++ lib/tab/templateSelectTab.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_example/component/templateContainer.dart';
+import 'package:flutter_example/tab/secondTab.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
 class TemplateSelectWidget extends StatelessWidget {
+  void _onTemplateSelect(int index, BuildContext context) {
+    print("call _onTemplateSelect()");
+    //Navigator.pushNamed(context, "/second");
+/*    Navigator.push(
+        context,
+        platformPageRoute(
+            builder: (_) {
+              return SecondTabPage();
+            },
+            context: context));*/
+  }
+
   @override
   Widget build(BuildContext context) {
     return PlatformScaffold(
@@ -10,15 +24,23 @@ class TemplateSelectWidget extends StatelessWidget {
         crossAxisCount: 2,
         children: new List.generate(16, (index) {
           return new GridTile(
-            child: new Card(
-                color: Colors.teal.shade200,
-                margin: EdgeInsets.symmetric(vertical: 7.0, horizontal: 7.0),
-                child: new Center(
-                  child: new Text(
-                    'Tile $index',
-                    style: TextStyle(fontFamily: 'Macadamia'),
-                  ),
-                )),
+            child: new TemplateContainer(
+              onTap: _onTemplateSelect,
+              containerIndex: index,
+              child: new Text(
+                'Tile $index',
+                style: TextStyle(fontFamily: 'Macadamia'),
+              ),
+            ), /*new Card(
+              color: Colors.teal.shade200,
+              margin: EdgeInsets.symmetric(vertical: 7.0, horizontal: 7.0),
+              child: new Center(
+                child: new Text(
+                  'Tile $index',
+                  style: TextStyle(fontFamily: 'Macadamia'),
+                ),
+              ),
+            ),*/
           );
         }),
       ),
