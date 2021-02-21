18ded807733a12f5cd0deb169ab16ca550bac2c1
dydtjr1128 <cys_star@naver.com>
2021-02-21 15:45:12 +0900

    Update: 페인터 성능 개선

--- lib/component/paintor/RoughyForegroundPainter.dart
+++ lib/component/paintor/RoughyForegroundPainter.dart
@@ -22,22 +22,39 @@ class RoughyForegroundPainter extends CustomPainter {
   void paint(Canvas canvas, Size size) async {
     ui.Paint paint = new ui.Paint()..strokeCap = StrokeCap.round;
     int len = points.length;
-    for (int i = 0; i < len - 1; i++) {
-      if (points[i] is RoughyDrawingPoint) {
-        if (points[i] != null && points[i + 1] != null && points[i+1] is RoughyDrawingPoint) {
+    for (int i = 0; i < len; i++) {
+      if (points[i] is RoughyDrawingPoint && points[i] != null && i + 1 < len) {
+        if (points[i + 1] != null && points[i + 1] is RoughyDrawingPoint) {
           paint
             ..color = points[i].color
             ..strokeWidth = points[i].depth;
           canvas.drawLine(points[i].offset, points[i + 1].offset, paint);
-        } else if (points[i] != null && points[i + 1] == null) {
+        } else if (points[i + 1] == null) {
           paint
             ..color = points[i].color
             ..strokeWidth = points[i].depth;
           canvas.drawPoints(ui.PointMode.points, [points[i].offset], paint);
         }
-      } else {
+      } else if (points[i] is RoughyTextPoint) {
+        RoughyTextPoint roughyTextPoint = points[i] as RoughyTextPoint;
         // RoughyTextPoint
-
+        final textStyle = TextStyle(
+            color: roughyTextPoint.color,
+            fontSize: roughyTextPoint.roughyFont.fontSize,
+            fontFamily: roughyTextPoint.roughyFont.fontName);
+        final textSpan = TextSpan(
+          text: roughyTextPoint.text,
+          style: textStyle,
+        );
+        final textPainter = TextPainter(
+          text: textSpan,
+          textDirection: TextDirection.ltr,
+        );
+        textPainter.layout(
+          minWidth: 0,
+          maxWidth: size.width,
+        );
+        textPainter.paint(canvas, roughyTextPoint.offset);
       }
     }
   }
