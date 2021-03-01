8be56ee66910741a87d4b561565c2e27e6ec80f4
dydtjr1128 <cys_star@naver.com>
2021-03-01 23:52:06 +0900

    Add: 엘범 템플릿에 글자 넣는 Input 페이지 창 구현

---
+++ lib/page/decorating/TextInputPage.dart
@@ -0,0 +1,55 @@
+import 'package:Roughy/component/roughyCenterAppBar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class TextInputPage extends StatefulWidget {
+  @override
+  _TextInputPageState createState() => _TextInputPageState();
+}
+
+class _TextInputPageState extends State<TextInputPage> {
+  final TextEditingController _textController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  setThisText() {
+    final form = _formKey.currentState;
+    print("@@@@" + form.validate().toString() + " " +  _textController.text.toString() + "!@@");
+    if (form.validate()) {
+      print("@@@ " + _textController.text);
+      Navigator.pop(context, _textController.text);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: RoughyCenterAppBar(
+          title: "Text",
+          isCenterTitle: true,
+          iconButton: new IconButton(
+            icon: new Icon(Icons.check),
+            tooltip: 'Save text',
+            onPressed: setThisText,
+          )),
+      body: Container(
+          child: Form(
+              key: _formKey,
+              child: new TextFormField(
+                  maxLines: 10,
+                  autofocus: true,
+                  controller: _textController,
+                  decoration: new InputDecoration(
+                      labelText: '텍스트',
+                      hintText: 'Please enter text',
+
+                      //border: InputBorder.none,
+                      contentPadding: const EdgeInsets.all(16.0)),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return "Please enter text";
+                    }
+                    return null;
+                  }))),
+    );
+  }
+}
