c135fb4bb16288c416dbe284ff352dc39579a06f
dydtjr1128 <cys_star@naver.com>
2021-03-21 17:46:25 +0900

    Update: 엘범 컨테이너 사이즈 오류 수정을 위해 전반적인 UI 리펙토링

--- lib/component/albumContainer.dart
+++ lib/component/albumContainer.dart
@@ -7,85 +7,66 @@ import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 class AlbumContainer extends StatefulWidget {
   final Function onTap;
   final int containerIndex;
-  final BorderRadius _baseBorderRadius = BorderRadius.circular(8);
+  final BorderRadius _baseBorderRadius = BorderRadius.circular(38);
   final String path;
 
-  AlbumContainer(
-      {@required this.onTap,
-      @required this.containerIndex,
-      @required this.path});
+  AlbumContainer({@required this.onTap, @required this.containerIndex, @required this.path});
 
   @override
   _AlbumContainerState createState() => _AlbumContainerState();
 }
 
 class _AlbumContainerState extends State<AlbumContainer> {
-  Future<File> _getLocalFile() async {
-    File f = new File(widget.path);
-    return f;
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(left: 5.0, right: 5.0, top: 5.0, bottom: 25.0),
-      child: InkWell(
-        onTap: () => widget.onTap(widget.containerIndex, widget.path, context),
-        child: Container(
-            decoration: BoxDecoration(
-              borderRadius: widget._baseBorderRadius,
-              color: Colors.transparent,
+    return Container(
+        //padding: EdgeInsets.only(left: 5.0, right: 5.0, top: 5.0, bottom: 25.0),
+        margin: EdgeInsets.only(left: 5.0, right: 5.0, top: 5.0, bottom: 25.0),
+        decoration: BoxDecoration(
+          borderRadius: widget._baseBorderRadius,
+          color: Colors.transparent,
+        ),
+        child: Stack(
+          children: [
+            Positioned.fill(
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                    ClipOval(
+                        child: Container(
+                      width: 20,
+                      height: 20,
+                      color: Colors.black,
+                      child: Center(
+                        child: PlatformText((widget.containerIndex + 1).toString(),
+                            style: TextStyle(
+                                fontFamily: 'SimplicityRegular',
+                                fontSize: 15,
+                                color: Colors.white)),
+                      ),
+                    )),
+                    Icon(context.platformIcons.bookmark)
+                  ]),
+                  Expanded(
+                      child: Container(
+                          margin: EdgeInsets.only(top: 10),
+                          decoration: new BoxDecoration(
+                            image:
+                                DecorationImage(fit: BoxFit.cover, image: AssetImage(widget.path)),
+                          )))
+                ],
+              ),
             ),
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      ClipOval(
-                          child: Container(
-                        width: 20,
-                        height: 20,
-                        color: Colors.black,
-                        child: Center(
-                          child: PlatformText(widget.containerIndex.toString(),
-                              style: TextStyle(
-                                  fontFamily: 'SimplicityRegular',
-                                  fontSize: 15,
-                                  color: Colors.white)),
-                        ),
-                      )),
-                      Icon(context.platformIcons.bookmark)
-                    ]),
-                Expanded(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      ClipRRect(
-                        child: new FutureBuilder(
-                            future: _getLocalFile(),
-                            builder: (BuildContext context,
-                                AsyncSnapshot<File> snapshot) {
-                              return snapshot.data != null
-                                  ? Container(
-                                      height:
-                                          MediaQuery.of(context).size.height,
-                                      margin: EdgeInsets.symmetric(
-                                          vertical: 15.0, horizontal: 0.0),
-                                      child: new Image.file(snapshot.data,
-                                          fit: BoxFit.fitWidth),
-                                    )
-                                  : new Container();
-                            }),
-                        //width:  MediaQuery.of(context).size.width,
-                      )
-                    ],
-                  ),
-                )
-              ],
-            )),
-      ),
-    );
+            new Positioned.fill(
+                child: new Material(
+                    color: Colors.transparent,
+                    child: new InkWell(
+                      //splashColor: Colors.lightGreenAccent,
+                      onTap: () => widget.onTap(widget.containerIndex, widget.path, context),
+                    ))),
+          ],
+        ));
   }
 }
