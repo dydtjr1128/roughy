0d1bdd2822003462506c34768d82606432ba7338
dydtjr1128 <cys_star@naver.com>
2021-04-05 21:53:09 +0900

    Update: 폰트 및 색상 설정할 수 있도록 합수 구현

--- lib/component/RoughyGestureTextController.dart
+++ lib/component/RoughyGestureTextController.dart
@@ -1,3 +1,9 @@
+import 'package:flutter/widgets.dart';
+
 class RoughyGestureTextController {
   void Function(bool isSelected) setWidgetSelected;
+  void Function(ScaleStartDetails details) onScaleStartHandler;
+  void Function(ScaleUpdateDetails details) onScaleUpdateHandler;
+  void Function(String fontName) setFont;
+  void Function(Color fontColor) setFontColor;
 }
