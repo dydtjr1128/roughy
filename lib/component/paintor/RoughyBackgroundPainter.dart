9df5cfad5928774445c9f58d16ea83e3ac5a12b2
dydtjr1128 <cys_star@naver.com>
2021-04-15 22:10:06 +0900

    Update: 러피 페인터 코드 리팩토링

--- lib/component/paintor/RoughyBackgroundPainter.dart
+++ lib/component/paintor/RoughyBackgroundPainter.dart
@@ -1,13 +1,26 @@
 import 'dart:ui' as ui;
 
 import 'package:flutter/widgets.dart';
+import 'dart:ui' as ui;
+
+import 'package:Roughy/data/RoughyData.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class RoughyBackgroundPainter extends CustomPainter {
   final ui.Image templateImage, croppedImage;
 
+  Color drawingColor;
+  double drawingDepth;
+
+  List<dynamic> points = [RoughyDrawingPoint, RoughyTextPoint];
+
   RoughyBackgroundPainter({
     @required this.templateImage,
     @required this.croppedImage,
+    @required this.points,
+    @required this.drawingColor,
+    @required this.drawingDepth,
   });
 
   @override
@@ -18,23 +31,39 @@ class RoughyBackgroundPainter extends CustomPainter {
     if (croppedImage != null) {
       canvas.drawImageRect(
           croppedImage,
-          Rect.fromLTRB(0, 0, croppedImage.width.toDouble(),
-              croppedImage.height.toDouble()),
+          Rect.fromLTRB(0, 0, croppedImage.width.toDouble(), croppedImage.height.toDouble()),
           Rect.fromLTRB(0, 0, width, height),
           Paint());
     }
     if (templateImage != null) {
       canvas.drawImageRect(
           templateImage,
-          Rect.fromLTRB(0, 0, templateImage.width.toDouble(),
-              templateImage.height.toDouble()),
+          Rect.fromLTRB(0, 0, templateImage.width.toDouble(), templateImage.height.toDouble()),
           Rect.fromLTRB(0, 0, width, height),
           Paint());
+    }
+
+    ui.Paint paint = new ui.Paint()..strokeCap = StrokeCap.round;
+    int len = points.length;
+    for (int i = 0; i < len &&  i+1 < len; i++) {
+      if (points[i] == null) {
+        continue;
+      } else if (points[i + 1] != null) {
+        paint
+          ..color = points[i].color
+          ..strokeWidth = points[i].depth;
+        canvas.drawLine(points[i].offset, points[i + 1].offset, paint);
+      } else if (points[i + 1] == null) {
+        paint
+          ..color = points[i].color
+          ..strokeWidth = points[i].depth;
+        canvas.drawPoints(ui.PointMode.points, [points[i].offset], paint);
+      }
     }
   }
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return false;
+    return true;
   }
 }
--- lib/component/paintor/RoughyForegroundPainter.dart
+++ lib/component/paintor/RoughyForegroundPainter.dart
@@ -20,39 +20,19 @@ class RoughyForegroundPainter extends CustomPainter {
   void paint(Canvas canvas, Size size) async {
     ui.Paint paint = new ui.Paint()..strokeCap = StrokeCap.round;
     int len = points.length;
-    for (int i = 0; i < len; i++) {
-      if (points[i] is RoughyDrawingPoint && points[i] != null && i + 1 < len) {
-        if (points[i + 1] != null && points[i + 1] is RoughyDrawingPoint) {
-          paint
-            ..color = points[i].color
-            ..strokeWidth = points[i].depth;
-          canvas.drawLine(points[i].offset, points[i + 1].offset, paint);
-        } else if (points[i + 1] == null) {
-          paint
-            ..color = points[i].color
-            ..strokeWidth = points[i].depth;
-          canvas.drawPoints(ui.PointMode.points, [points[i].offset], paint);
-        }
-      } else if (points[i] is RoughyTextPoint) {
-        /*RoughyTextPoint roughyTextPoint = points[i] as RoughyTextPoint;
-        // RoughyTextPoint
-        final textStyle = TextStyle(
-            color: roughyTextPoint.color,
-            fontSize: roughyTextPoint.roughyFont.fontSize,
-            fontFamily: roughyTextPoint.roughyFont.fontName);
-        final textSpan = TextSpan(
-          text: roughyTextPoint.text,
-          style: textStyle,
-        );
-        final textPainter = TextPainter(
-          text: textSpan,
-          textDirection: TextDirection.ltr,
-        );
-        textPainter.layout(
-          minWidth: 0,
-          maxWidth: size.width,
-        );
-        textPainter.paint(canvas, roughyTextPoint.offset);*/
+    for (int i = 0; i < len &&  i+1 < len; i++) {
+      if (points[i] == null) {
+        continue;
+      } else if (points[i + 1] != null) {
+        paint
+          ..color = points[i].color
+          ..strokeWidth = points[i].depth;
+        canvas.drawLine(points[i].offset, points[i + 1].offset, paint);
+      } else if (points[i + 1] == null) {
+        paint
+          ..color = points[i].color
+          ..strokeWidth = points[i].depth;
+        canvas.drawPoints(ui.PointMode.points, [points[i].offset], paint);
       }
     }
   }
