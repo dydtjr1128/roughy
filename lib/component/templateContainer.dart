7ed8d3e7dbaa8d3d7dd0f738ab153870aa426030
dydtjr1128 <cys_star@naver.com>
2021-05-27 17:46:26 +0900

    Update: Favorite 아이콘 적용

--- lib/component/templateContainer.dart
+++ lib/component/templateContainer.dart
@@ -1,7 +1,7 @@
 import 'package:Roughy/data/Template.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:flutter_svg/svg.dart';
 
 class TemplateContainer extends StatefulWidget {
   final Function onTap;
@@ -28,8 +28,15 @@ class _TemplateContainerState extends State<TemplateContainer> {
     _isFavoriteSelected = widget.template.isFavorite;
     return Card(
       elevation: 0,
-      color: Color.fromRGBO(235, 235, 235, 1),
-      shape: RoundedRectangleBorder(borderRadius: widget._baseBorderRadius),
+      color: Color.fromRGBO(255, 255, 255, 1),
+      //shadowColor: Colors.blue,
+      shape: RoundedRectangleBorder(
+        borderRadius: widget._baseBorderRadius,
+        side: BorderSide(
+          color: Colors.grey,
+          width: 0.5,
+        ),
+      ),
       margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 5.0),
       child: InkWell(
           borderRadius: widget._baseBorderRadius,
@@ -44,12 +51,10 @@ class _TemplateContainerState extends State<TemplateContainer> {
                   ClipRRect(
                       child: Center(
                     child: Container(
-                      child: Image.asset(
-                          "assets/templates/${widget.template.imageName}",
+                      child: Image.asset("assets/templates/${widget.template.imageName}",
                           fit: BoxFit.contain),
                       height: MediaQuery.of(context).size.height,
-                      margin:
-                          EdgeInsets.symmetric(vertical: 15.0, horizontal: 0.0),
+                      margin: EdgeInsets.symmetric(vertical: 15.0, horizontal: 0.0),
                     ),
                   )),
                   new Positioned(
@@ -62,20 +67,19 @@ class _TemplateContainerState extends State<TemplateContainer> {
                               crossAxisAlignment: CrossAxisAlignment.center,
                               children: <Widget>[
                             GestureDetector(
-                                onTap: () {
-                                  setState(() {
-                                    _isFavoriteSelected = !_isFavoriteSelected;
-                                    print(_isFavoriteSelected);
-                                    widget.onFavoriteTap(widget.containerIndex,
-                                        _isFavoriteSelected, context);
-                                  });
-                                },
-                                child: _isFavoriteSelected
-                                    ? Icon(context.platformIcons.favoriteSolid,
-                                        color: Colors.red)
-                                    : Icon(
-                                        context.platformIcons.favoriteOutline,
-                                        color: Colors.red)),
+                              onTap: () {
+                                setState(() {
+                                  _isFavoriteSelected = !_isFavoriteSelected;
+                                  print(_isFavoriteSelected);
+                                  widget.onFavoriteTap(
+                                      widget.containerIndex, _isFavoriteSelected, context);
+                                });
+                              },
+                              child: SvgPicture.asset('assets/icons/for_you.svg',
+                                  color: _isFavoriteSelected
+                                      ? Color.fromRGBO(228, 231, 233, 1)
+                                      : Color.fromRGBO(212, 244, 250, 1)),
+                            ),
                           ]))),
                 ],
               ))),
